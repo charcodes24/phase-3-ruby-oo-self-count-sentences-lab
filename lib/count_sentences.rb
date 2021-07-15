7ab5c51bc8993786178a302c080c70f5ff704b91
charcodes24 <charcodes24@gmail.com>
2021-07-15 11:17:46 -0700

    commit tree efb40899a601e3b7f6cd0d9b3cbf004082b7dc74

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    array_sentences = self.split(" ")
+    array_sentences.each {|word| count+=1 if word.sentence? || word.question? || word.exclamation?}
+    count
   end
 end
